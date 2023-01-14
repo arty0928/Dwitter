162382c1569e87ffcad2b3d97dfbaf306b9d84fa
arty0928 <arty0928@konkuk.ac.kr>
2023-01-15 00:43:34 +0900

    22.1.13 Login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,20 @@ import {StreamChat} from "stream-chat";
 import {Chat} from "stream-chat-react";
 import Cookies from "universal-cookie";
 
-import {ChannelListContainer, ChannelContainer} from './components';
+import {ChannelListContainer, ChannelContainer, Auth} from './components';
+
+import './App.css';
 
 const apiKey = 'fx6t7yf37tsn';
 
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 const App = () => {
-  return (
+    if(!authToken) return <Auth />
+
+    return (
     <div className='app__wrapper'>
         <Chat client={client} theme="team light">
             <ChannelListContainer
@@ -21,7 +27,7 @@ const App = () => {
             />
         </Chat>
     </div>
-  );
+    );
 }
 
 export default App;
