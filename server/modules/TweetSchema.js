4a1b6dbd9127d718ca9174ed6cd7eeafe4d46135
Luna-rmsoft <88071251+arty0928@users.noreply.github.com>
2023-02-23 17:58:14 +0900

    "mine"

---
+++ server/modules/TweetSchema.js
@@ -0,0 +1,9 @@
+const TweetSchema = {
+    id: string,  // 트윗 아이디
+    text: string,  // 트윗 텍스트
+    createdAt: Date, // 트윗 생성 날짜
+    name: string,  // 사용자 이름
+    username: string,  // 사용자 닉네임 (아이디)
+    url: string (optional) // 사용자 프로파일 사진 URL
+};
+
