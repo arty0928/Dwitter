583841c8f1c8143af7600e9f46ce135c2a5c508e
arty0928 <arty0928@konkuk.ac.kr>
2023-03-08 11:17:15 +0900

    "login-before compare pw"

--- server/controller/user.js
+++ server/controller/user.js
@@ -2,23 +2,65 @@ import * as userRepository from '../data/user.js';
 
 export async function signup(req, res) {
     //username, password, name, email, url
-    console.log(req.body);
+    const checkUser = await userRepository.findUsername(req.body);
 
-    const user = await userRepository.bcryptPassword(req.body);
+    //checkUser = undefined 면 
+    //즉, username 이 중복이 아니면
+    if(checkUser){
+        res.status(404).json({
+            signupSuccess: false,
+            message: "username 중복"
+        })
+    }
+    else{
+        const user = await userRepository.signup(req.body);
 
-    if(!user) res.status(404).json({
-        signupSuccess: false
-    })
-
-    res.status(200).json({
-        signupSuccess: true,
-        token: user.token,
-        username: user.username
-    })
+        if(!user) res.status(404).json({
+            signupSuccess: false
+        })
+        else{
+            res.status(200).json({
+                signupSuccess: true,
+                token: user.token,
+                username: user.username
+            })
+        }
+    }
 }
 
 export async function login(req, res) {
+    //request: username, password
+    console.log('login req');
+    console.log(req.body);
 
+    
+    //일치하는 username이 있는지 확인
+    const user = await userRepository.findUsername(req.body.username);
+        //비밀번호 비교
+
+    if(user){
+        const comparePassword = await userRepository.comparePassword(user, req.body.password);
+        if(comparePassword){
+            res.status(404).json({
+                loginSuccess: true,
+                username: user.username,
+                token: user.token
+            })
+        }else{
+            res.status(404).json({
+                loginSuccess: false,
+                message: "비밀번호 틀림"
+            })
+        }
+        
+    }
+    else{
+        res.status(404).json({
+            loginSuccess: false,
+            message: "username 없음"
+        })
+    }
+    //response: token, username
 }
 
 export async function me(req, res){
--- server/data/user.js
+++ server/data/user.js
@@ -9,10 +9,28 @@ let users = [
         email: 'ellie@gmail.com',
         url: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
     },
+    {
+        username: "black",
+        password : "2222",
+        name : "black",
+        email : "black@gmail.com",
+        url: ""
+    }
 ];
 
-export async function bcryptPassword(userdata){
-    let user = {
+export async function findUsername(username){
+
+    console.log(users);
+    const user = users.find((user) => user.username === username);
+
+    console.log(`find user`);
+    console.log(user);
+    return user;
+}
+
+export async function signup(userdata){
+
+    const user = {
         username: userdata.username,
         password: userdata.password,
         name:  userdata.name,
@@ -40,9 +58,22 @@ export async function bcryptPassword(userdata){
     },'secretKey');
 
     user.token = token;
-    console.log(user);
+    
+    console.log(`before users`);
+    console.log(users);
 
     users = [user, ...users];
 
+    console.log(`after users`);
+    console.log(users);
     return  user;
+}
+
+
+
+export async function comparePassword(user, password){
+    // users.comparePassword(password, (err, isMatch) =>{
+    //     if()
+    // })
+    // return (user.password === password ? true : false);
 }
