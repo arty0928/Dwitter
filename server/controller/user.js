bbfd024866fcf60532a7db6998e821605b60a123
arty0928 <arty0928@konkuk.ac.kr>
2023-03-08 09:35:42 +0900

    "signup"

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,54 +1,18 @@
 import * as userRepository from '../data/user.js';
 
-import jwt from 'jsonwebtoken';
-import bcryt from 'bcrypt';
-
 export async function signup(req, res) {
     //username, password, name, email, url
     console.log(req.body);
-    //이미 회원가입한 계정인지 이메일 비교,
-    let user = {
-        username: req.body.username,
-        password: req.body.password,
-        name:  req.body.name,
-        email:  req.body.email,
-        url:  req.body.url
-    };
-    //비밀번호 비크립트화해서 저장
-    bcryt.genSalt(10,function(err,salt){
-        if(err) return Error(err);
 
-        console.log(`salt: ${salt}`);
+    const user = await userRepository.bcryptPassword(req.body);
 
-        bcryt.hash(user.password, salt, function(err,hash){
-            if(err) return Error(err);
-
-            user.password = hash;
-            console.log(user.password);
-        })
+    if(!user) res.status(404).json({
+        signupSuccess: false
     })
 
-    //token 발행 후
-    const token = jwt.sign({
-        //payload는 담고싶은 중요한 정보
-        id: user.id,
-        isAdmin : true,
-    },
-        //secretKey (랜덤문자열)
-        'secretKey'
-    );
-
-    console.log(`token: ${token}`);
-    //response : token, username
-
-    user.token = token;
-
-    
-
-    console.log(user);
     res.status(200).json({
         signupSuccess: true,
-        token,
+        token: user.token,
         username: user.username
     })
 }
--- server/data/user.js
+++ server/data/user.js
@@ -1,3 +1,6 @@
+import jwt from 'jsonwebtoken';
+import bcryt from 'bcrypt';
+
 let users = [
     {
         username: 'Ellie',
@@ -7,3 +10,39 @@ let users = [
         url: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
     },
 ];
+
+export async function bcryptPassword(userdata){
+    let user = {
+        username: userdata.username,
+        password: userdata.password,
+        name:  userdata.name,
+        email:  userdata.email,
+        url:  userdata.url
+    };
+    
+    bcryt.genSalt(10,function(err,salt){
+        if(err) return Error(err);
+
+        console.log(`salt: ${salt}`);
+
+        bcryt.hash(user.password, salt, function(err,hash){
+            if(err) return Error(err);
+
+            user.password = hash;
+            console.log(user.password);
+        })
+    })
+
+    const token = jwt.sign({
+        //payload는 담고싶은 중요한 정보
+        id: user.id,
+        isAdmin : true,
+    },'secretKey');
+
+    user.token = token;
+    console.log(user);
+
+    users = [user, ...users];
+
+    return  user;
+}
